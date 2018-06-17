afa20873e38bfe90b74f3c0fecdd9d53972574dc
Ashok S Nair <40341278+ashok-s-nair@users.noreply.github.com>
2018-06-18 00:35:49 +0530

    Added the CardSuit module.

---
+++ app/cardsuit.js
@@ -0,0 +1,13 @@
+/**
+ * Defines a map outlining the suits of the card in the deck.
+ */
+'use strict';
+
+const CardSuit = new Map([
+    ['DIAMONDS', 0],
+    ['CLUBS', 1],
+    ['HEARTS', 2],
+    ['SPADES', 3]
+]);
+
+module.exports = CardSuit;
